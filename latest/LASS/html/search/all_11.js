cc015f35c4bd8e4a930380c65c4f9eb30fe05e50
passyur <passyur@users.noreply.github.com>
2026-05-30 18:14:35 +0000

    docs: publish latest (ef7613b64755b54cd50adbbd5a1d94a43f6c6af1) ef7613b64755b54cd50adbbd5a1d94a43f6c6af1

--- latest/LASS/html/search/all_11.js
+++ latest/LASS/html/search/all_11.js
@@ -1,15 +1,19 @@
 var searchData=
 [
-  ['readtag_0',['readTag',['../classXmlReader.html#ae40848fcb60577d56f1c035105199195',1,'XmlReader']]],
-  ['readxmldocument_1',['readxmldocument',['../classXmlReader.html#a91747db64d33ec13bd30fa023899845c',1,'XmlReader::readXMLDocument()'],['../classXmlReader.html#a10b70989e8b704fed7974ca01266cb2f',1,'XmlReader::readXMLDocument(xmltag *)']]],
-  ['relative_5famplitude_2',['RELATIVE_AMPLITUDE',['../Partial_8h.html#a62dc27a60fb5ec2fdcd248cea649f681a23ab70532c631b91fff66923a8aa334f',1,'Partial.h']]],
-  ['remove_3',['remove',['../classCollection.html#a5eee5da64607d3bce3bc70ab840b6b88',1,'Collection']]],
-  ['render_4',['render',['../Score_8cpp.html#a39d69126a1c6f253e4f8e8dbe365ee1d',1,'render():&#160;Score.cpp'],['../classPartial.html#a6ab80d7069346d873d3811de427a53d8',1,'Partial::render()'],['../classSound.html#a4f246242d337ab7ef8a646f69f2a04ae',1,'Sound::render()']]],
-  ['report_5',['Report',['../classMemoryLeak.html#aa7ae45b11c7f3962ed21c99725cc24c5',1,'MemoryLeak']]],
-  ['reset_6',['reset',['../classAllPassFilter.html#a7d2526a71f27ef869724259ee834efc9',1,'AllPassFilter::reset()'],['../classReverb.html#a8e5b74b8cd84a26019b3d4759c94bf3f',1,'Reverb::reset()'],['../classLPCombFilter.html#a2b8839961610ed9fea1d04f68d3889e1',1,'LPCombFilter::reset()'],['../classLowPassFilter.html#a20c94f3752e64d7a5691c26c3471dd82',1,'LowPassFilter::reset()'],['../classFilter.html#a257e4310e9c1dd4279d14fa5c341cbfc',1,'Filter::reset()'],['../classBiQuadFilter.html#aa56894dac418476549f8ad1aca0b2af2',1,'BiQuadFilter::reset()']]],
-  ['reverb_7',['reverb',['../classReverb.html#af7cd1f214ec64221a690e4fb53a87770',1,'Reverb::Reverb(m_rate_type samplingRate)'],['../classReverb.html#a9306c9e088770c77de1aebfdd8d54845',1,'Reverb::Reverb(float room_size, m_rate_type samplingRate)'],['../classReverb.html#aae078d70ce96505cc0b45218d4826044',1,'Reverb::Reverb(Envelope *percentReverb, float hilow_spread, float gainAllPass, float delay, m_rate_type samplingRate)'],['../classReverb.html#a9dda18325ae72043bfb06e0b3377da8c',1,'Reverb::Reverb(Envelope *percentReverb, float *combGainList, float *lpGainList, float gainAllPass, float delay, m_rate_type samplingRate)'],['../classReverb.html',1,'Reverb']]],
-  ['reverb_2ecpp_8',['Reverb.cpp',['../Reverb_8cpp.html',1,'']]],
-  ['reverb_2eh_9',['Reverb.h',['../Reverb_8h.html',1,'']]],
-  ['reverb_5fnum_5fcomb_5ffilters_10',['REVERB_NUM_COMB_FILTERS',['../Reverb_8h.html#afea42ca0cd96b2023d112572d0b319e8',1,'Reverb.h']]],
-  ['reverbhash_11',['reverbHash',['../classScore.html#a844c2e28463173a242fe3a0fedd6ef91',1,'Score']]]
+  ['raw_5farray_0',['raw_array',['../classFilter_1_1hist__queue.html#a207ac3e8cf1427208be745fb8593bc1b',1,'Filter::hist_queue']]],
+  ['raw_5fback_1',['raw_back',['../classFilter_1_1hist__queue.html#a10d6dddcc3ee4cdce7f2ec92bbcdb9b4',1,'Filter::hist_queue']]],
+  ['raw_5ffront_2',['raw_front',['../classFilter_1_1hist__queue.html#a5132210d4055da38a909d0fd58022eea',1,'Filter::hist_queue']]],
+  ['raw_5flength_3',['raw_length',['../classFilter_1_1hist__queue.html#a9028e764ad5dad7c7ed77e502ff827d5',1,'Filter::hist_queue']]],
+  ['readtag_4',['readTag',['../classXmlReader.html#ae40848fcb60577d56f1c035105199195',1,'XmlReader']]],
+  ['readxmldocument_5',['readxmldocument',['../classXmlReader.html#a91747db64d33ec13bd30fa023899845c',1,'XmlReader::readXMLDocument()'],['../classXmlReader.html#a10b70989e8b704fed7974ca01266cb2f',1,'XmlReader::readXMLDocument(xmltag *)']]],
+  ['relative_5famplitude_6',['RELATIVE_AMPLITUDE',['../Partial_8h.html#a62dc27a60fb5ec2fdcd248cea649f681a23ab70532c631b91fff66923a8aa334f',1,'Partial.h']]],
+  ['remove_7',['remove',['../classCollection.html#a5eee5da64607d3bce3bc70ab840b6b88',1,'Collection']]],
+  ['render_8',['render',['../classSound.html#a4f246242d337ab7ef8a646f69f2a04ae',1,'Sound::render()'],['../classPartial.html#a6ab80d7069346d873d3811de427a53d8',1,'Partial::render()'],['../Score_8cpp.html#a39d69126a1c6f253e4f8e8dbe365ee1d',1,'render():&#160;Score.cpp']]],
+  ['report_9',['Report',['../classMemoryLeak.html#aa7ae45b11c7f3962ed21c99725cc24c5',1,'MemoryLeak']]],
+  ['reset_10',['reset',['../classAllPassFilter.html#a7d2526a71f27ef869724259ee834efc9',1,'AllPassFilter::reset()'],['../classBiQuadFilter.html#aa56894dac418476549f8ad1aca0b2af2',1,'BiQuadFilter::reset()'],['../classFilter.html#a257e4310e9c1dd4279d14fa5c341cbfc',1,'Filter::reset()'],['../classLowPassFilter.html#a20c94f3752e64d7a5691c26c3471dd82',1,'LowPassFilter::reset()'],['../classLPCombFilter.html#a2b8839961610ed9fea1d04f68d3889e1',1,'LPCombFilter::reset()'],['../classReverb.html#a8e5b74b8cd84a26019b3d4759c94bf3f',1,'Reverb::reset(void)']]],
+  ['reverb_11',['reverb',['../classReverb.html#af7cd1f214ec64221a690e4fb53a87770',1,'Reverb::Reverb(m_rate_type samplingRate)'],['../classReverb.html#a9306c9e088770c77de1aebfdd8d54845',1,'Reverb::Reverb(float room_size, m_rate_type samplingRate)'],['../classReverb.html#aae078d70ce96505cc0b45218d4826044',1,'Reverb::Reverb(Envelope *percentReverb, float hilow_spread, float gainAllPass, float delay, m_rate_type samplingRate)'],['../classReverb.html#a9dda18325ae72043bfb06e0b3377da8c',1,'Reverb::Reverb(Envelope *percentReverb, float *combGainList, float *lpGainList, float gainAllPass, float delay, m_rate_type samplingRate)'],['../classReverb.html',1,'Reverb']]],
+  ['reverb_2ecpp_12',['Reverb.cpp',['../Reverb_8cpp.html',1,'']]],
+  ['reverb_2eh_13',['Reverb.h',['../Reverb_8h.html',1,'']]],
+  ['reverb_5fnum_5fcomb_5ffilters_14',['REVERB_NUM_COMB_FILTERS',['../Reverb_8h.html#afea42ca0cd96b2023d112572d0b319e8',1,'Reverb.h']]],
+  ['reverbhash_15',['reverbHash',['../classScore.html#a844c2e28463173a242fe3a0fedd6ef91',1,'Score']]]
 ];
