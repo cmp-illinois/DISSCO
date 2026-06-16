9812d53b5b28b32cce355bb75108133f7eb02112
leyiQ <qianleyi0427@hotmail.com>
2026-06-16 17:39:07 -0500

    gray out number of partials in spectrum

--- LASSIE/src/widgets/EventAttributesViewController.cpp
+++ LASSIE/src/widgets/EventAttributesViewController.cpp
@@ -766,6 +766,7 @@ void EventAttributesViewController::showCurrentEventData() {
             ui->soundNameEntry->setText(event.name);
             ui->soundNameEntry->setEnabled(false);
             ui->spectrumNumPartialEntry->setText(event.num_partials);
+            //ui->spectrumNumPartialEntry->setEnabled(false);
             ui->spectrumDeviationEntry->setText(event.deviation);
             ui->spectrumGenEntry->setText(event.generate_spectrum);
 
